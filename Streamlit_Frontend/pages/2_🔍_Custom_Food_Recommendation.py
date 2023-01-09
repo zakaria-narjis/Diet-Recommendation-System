24c1be1929ab6956e9cfdf5d03e0cfc2ccd31c80
Zakaria Narjis <zakaria.narjis.97@gmail.com>
2023-01-09 02:32:49 +0100

    changed layout to wide

--- Streamlit_Frontend/pages/2_🔍_Custom_Food_Recommendation.py
+++ Streamlit_Frontend/pages/2_🔍_Custom_Food_Recommendation.py
@@ -2,4 +2,4 @@ import streamlit as st
 import time
 import numpy as np
 
-st.set_page_config(page_title="Plotting Demo", page_icon="🔍")
+st.set_page_config(page_title="Plotting Demo", page_icon="🔍",layout="wide")
