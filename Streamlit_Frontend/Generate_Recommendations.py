ace0d0a639a2467f44d3fb066e32441cb9330699
Zakaria Narjis <zakaria.narjis.97@gmail.com>
2023-01-09 02:31:36 +0100

    added front end recommendation generator

---
+++ Streamlit_Frontend/Generate_Recommendations.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import pandas as pd
+
+class Generator:
+    def __init__(self,nutrition_input:list,ingredients:list=[],params:dict={'n_neighbors':5,'return_distance':False}):
+        self.nutrition_input=nutrition_input
+        self.ingredients=ingredients
+        self.params=params
+
+    def set_request(self,nutrition_input:list,ingredients:list,params:dict):
+        self.nutrition_input=nutrition_input
+        self.ingredients=ingredients
+        self.params=params
+
+    def generate(self,):
+        request={
+            'nutrition_input':self.nutrition_input,
+            'ingredients':self.ingredients,
+            'params':self.params
+        }
+        response=requests.post(url='http://127.0.0.1:8000/predict/',data=json.dumps(request))
+        return response
+
+
+gen=Generator([100,10,10,10,10,10,10,10,10])
+rep=gen.generate()
+df=pd.DataFrame(data=eval(rep.text)['output'])
+print(df.info())
