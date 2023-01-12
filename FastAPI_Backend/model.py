57dfb04977fad5267bd28268690c47e28a1bff72
Zakaria Narjis <zakaria.narjis.97@gmail.com>
2023-01-12 16:41:42 +0100

    correcting API recommendation function

--- FastAPI_Backend/model.py
+++ FastAPI_Backend/model.py
@@ -36,9 +36,9 @@ def apply_pipeline(pipeline,_input,extracted_data):
     _input=np.array(_input).reshape(1,-1)
     return extracted_data.iloc[pipeline.transform(_input)[0]]
 
-def recommend(dataframe,_input,ingredients=[],params={'return_distance':False}):
+def recommend(dataframe,_input,ingredients=[],params={'n_neighbors':5,'return_distance':False}):
         extracted_data=extract_data(dataframe,ingredients)
-        if extracted_data.shape[0]!=0:
+        if extracted_data.shape[0]>=params['n_neighbors']:
             prep_data,scaler=scaling(extracted_data)
             neigh=nn_predictor(prep_data)
             pipeline=build_pipeline(neigh,scaler,params)
@@ -53,11 +53,13 @@ def extract_quoted_strings(s):
     return strings
 
 def output_recommended_recipes(dataframe):
-    output=dataframe.copy()
-    if output is not None:
+    if dataframe is not None:
+        output=dataframe.copy()
         output=output.to_dict("records")
         for recipe in output:
             recipe['RecipeIngredientParts']=extract_quoted_strings(recipe['RecipeIngredientParts'])
             recipe['RecipeInstructions']=extract_quoted_strings(recipe['RecipeInstructions'])
+    else:
+        output=None
     return output
 
