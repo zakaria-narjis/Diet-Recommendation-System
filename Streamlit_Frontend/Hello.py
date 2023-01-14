ec0c42d30d236d6c7f9dd148e718530b19ce9f7f
Zakaria Narjis <zakaria.narjis.97@gmail.com>
2023-01-14 18:34:24 +0100

    update text main page

--- Streamlit_Frontend/Hello.py
+++ Streamlit_Frontend/Hello.py
@@ -12,6 +12,6 @@ st.sidebar.success("Select a recommendation app.")
 st.markdown(
     """
     A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
-    You can find more details and the whole project on my [repo](https://github.com/zakaria-narjis/Diet-Recommendation-System)
+    You can find more details and the whole project on my [repo](https://github.com/zakaria-narjis/Diet-Recommendation-System).
     """
 )
